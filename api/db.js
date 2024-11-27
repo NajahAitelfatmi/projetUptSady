498a413c63755d57bcb966685c38ac89cf02d4ff
nexus innovations <nexus.innovations2024@gmail.com>
2024-11-27 11:59:12 +0100

    add modification

--- api/db.js
+++ api/db.js
@@ -1,5 +1,6 @@
 // Charger dotenv pour accéder aux variables d'environnement
-require('dotenv').config();
+import dotenv from 'dotenv';
+dotenv.config();
 
 import mysql from "mysql"
 
@@ -9,3 +10,13 @@ export const db = mysql.createConnection({
   password: process.env.DB_KEY,
   database:"blog"
 })
+// Vérifier si la connexion est réussie
+db.connect((err) => {
+  if (err) {
+      console.error('Erreur de connexion à la base de données:', err.stack);
+      return;
+  }
+  console.log('Connecté à la base de données MySQL avec l\'ID', db.threadId);
+});
+
+export default db;
