00f45d1bbe6847a06ec8f5476e34f0c0f14981a9
dinaelhyate <dinaelhyate@gmail.com>
2024-11-30 11:06:06 +0100

    add contact api

--- api/index.js
+++ api/index.js
@@ -4,6 +4,7 @@ import userRoutes from "./routes/users.js";
 import postRoutes from "./routes/posts.js";
 import cookieParser from "cookie-parser";
 import multer from "multer";
+import nodemailer from 'nodemailer';
 
 const app = express();
 
@@ -25,6 +26,45 @@ app.post("/api/upload", upload.single("file"), function (req, res) {
   const file = req.file;
   res.status(200).json(file.filename);
 });
+
+
+app.post('/api/contact', async (req, res) => {
+  const { name, email, subject, message } = req.body;
+
+  // Validation des champs requis
+  if (!name || !email || !subject || !message) {
+      return res.status(400).json({ error: 'Tous les champs sont obligatoires.' });
+  }
+
+  try {
+      const transporter = nodemailer.createTransport({
+          host: 'smtp.gmail.com',
+          port: 587,
+          secure: false, // Utilise TLS
+          auth: {
+              user: 'dinaelhyate@gmail.com', // Votre adresse Gmail
+              pass: 'fvlrnrdnfcffopyy', // Mot de passe d'application
+          },
+          tls: {
+              rejectUnauthorized: false, // Permet les certificats auto-signés
+          },
+      });
+
+      // Utiliser l'email fourni dans le formulaire pour le destinataire
+      await transporter.sendMail({
+          from: `"${name}" <${email}>`, // Expéditeur
+          to: 'dinaelhyate@gmail.com', // Destinataire (utilisé depuis l'input)
+          subject: subject, // Sujet
+          text: message, // Message
+      });
+
+      res.status(200).json({ message: 'Message envoyé avec succès.' });
+  } catch (error) {
+      console.error('Erreur lors de l\'envoi du message:', error);
+      res.status(500).json({ error: 'Erreur lors de l\'envoi du message.' });
+  }
+});
+
 
 app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
